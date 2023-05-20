0dcd1d0af6b6cc395ee1bf752a27db53e9234350
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2023-05-20 14:54:08 -0400

    userpage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import GithubLoginScreen from "./screens/GithubLoginScreen";
 import ProfilePage from "./screens/Profile";
 import Landing from "./screens/Landing";
+import UserPage from "./screens/ProfilePage";
 
 function App() {
   // forward the user to the github login screen (we pass in the client ID)
@@ -16,6 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing/>}/>
         <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/" element={<GithubLoginScreen/>}/>
+        <Route path="/profilepage" element={<ProfilePage/>}/>
+        <Route path=":userId" element={<UserPage />}/>
       </Routes>
     </>
   );
