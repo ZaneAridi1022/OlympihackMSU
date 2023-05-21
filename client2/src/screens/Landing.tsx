22b1046a4c7ae71ce8cea550fd01c55fdb4d1b30
Sungyuk1 <kwonsun7@msu.edu>
2023-05-21 01:03:00 -0400

    Got Tailwind Working

--- client2/src/screens/Landing.tsx
+++ client2/src/screens/Landing.tsx
@@ -4,7 +4,13 @@ import Taskbar from "../components/Taskbar/Taskbar";
 
 function Landing() {
   return (
-    <Taskbar />
+    <>
+      <Taskbar />
+      <div className="flex flex-col flex-grow bg-black text-white">
+        <h1>Welcome to TrustLink</h1>
+
+      </div>
+    </>
   );
 }
 
